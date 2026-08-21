7e6e7f8be5724ec9cbf1f3001c9cac9874692b8b
Dhinesh-Creator25 <dhineshcreater@gmail.com>
2026-08-21 23:59:44 +0530

    Fix_ENETUNREACH_IPv6_network_issue_on_Render_by_forcing_ipv4first

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,3 +1,8 @@
+const dns = require("dns");
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder("ipv4first");
+}
+
 const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
 const mobileProducts = require("./mobileProducts");
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,8 @@
+const dns = require("dns");
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder("ipv4first");
+}
+
 require("dotenv").config();
 
 const bcrypt = require("bcryptjs");
